921695ed830ad2b08ae67418e8c0c5e0c032e531
Wahaj Ahmed <wahaj465@gmail.com>
2019-11-06 03:18:56 +0500

    second comit 4.42

--- MyApp/models.py
+++ MyApp/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 class search(models.Model):
     search = models.CharField(max_length=500)
-    created = models.DateTimeField(auto_now=true)
+    created = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    class Meta:
+        verbose_name_plural = 'Searches'
--- MyApp/urls.py
+++ MyApp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
+
 
 urlpatterns = [
-    path('',views.home , name='home'),
-    path('admin/' , admin.site.urls),
+    path('' , views.home , name='home'),
 ]
