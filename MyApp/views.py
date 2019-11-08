479340dabcc12675d84c33cbbd4d4afc2d523ddb
Wahaj Ahmed <wahaj465@gmail.com>
2019-11-08 18:55:47 +0500

    third comit 5.24

--- MyApp/views.py
+++ MyApp/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 def home(request):
     return render(request ,'base.html')
  
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend = {
+        'search' : search
+    }
+    return render(request, 'new_search.html ' , stuff_for_frontend  )
